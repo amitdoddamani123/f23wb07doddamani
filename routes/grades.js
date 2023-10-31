af0816ed6c50c360a0e226b5bc6964cc05edf6df
amitdoddamani123 <amit.rcb95@gmail.com>
2023-10-31 18:45:49 -0500

    added board files

--- routes/grades.js
+++ routes/grades.js
@@ -4,6 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('grades', { title: 'Search Results Grades' });
+
+ 
+
 });
 
 module.exports = router;
